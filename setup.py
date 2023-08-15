5125624c3ffd659001ffdc2e7b752c106c9fb5bf
juanMaAV92 <juanmanuel.armero@gmail.com>
2023-08-15 14:58:24 -0500

    tag v0.1.0

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ from setuptools import setup, find_packages
 setup(
     name="cripto-utils-lib",
     version='0.1.0',
+    author="Juan Manuel Armero Viveros",
+    author_email="juanmanuel.armero@gmail.com",
+    url="https://github.com/juanMaAV92/cripto-utils-lib",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     packages= find_packages(),
     install_requires=[
 
